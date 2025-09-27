d3ebd932ceda8b7a7b2b31364f460e9e40e0cfe2
akumar <ashwin6323@gmail.com>
2025-09-27 17:01:02 +0530

    Carousel image fixed for small devices

--- src/components/customCarousel.tsx
+++ src/components/customCarousel.tsx
@@ -42,7 +42,7 @@ function CustomCarousel({ items, isHomePage }: IProps) {
               <img
                 src={item.source}
                 alt={`Slide ${index + 1}`}
-                className={isHomePage ? 'home-page-slide' : ''}
+                className={isHomePage ? 'home-page-slide' : 'responsive-slide'}
               />
             ) : (
               <video
